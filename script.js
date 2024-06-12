ede2431d9cf7ffde5f558c741eef5cbed1e6b8e6
deepak01112002 <deepak8574757609@gmail.com>
2024-06-12 17:56:30 +0530

    add

---
+++ script.js
@@ -0,0 +1,17 @@
+// while(condition){
+
+// }
+
+// do{
+
+// }while(condtion)
+
+let a = 0; 
+while(a<0){
+    console.log("Hello")
+    a++;
+}
+
+// do{
+//   console.log("Hello")
+// }while(a<0)
