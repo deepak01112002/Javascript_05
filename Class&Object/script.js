cf4cb66ba8ae4f9a6f76ce66a5a8dfa6be64593a
deepak01112002 <deepak8574757609@gmail.com>
2024-08-09 17:55:51 +0530

    class&object

---
+++ Class&Object/script.js
@@ -0,0 +1,35 @@
+let student1 = {
+    name : "ashirwad",
+    GRID : 5105,
+    subject : "ai"
+}
+let student2 = {
+    name : "Dhol",
+    GRID : 420,
+    subject : "accounting"
+}
+
+
+class Students{
+    constructor(name,GRID,subject,math,english){
+        this.name = name,
+        this.GRID = GRID,
+        this.subject = subject
+        this.math = math,
+        this.english = english
+    }
+    getGRID(){
+        console.log(`${this.name} GRID is ${this.GRID}`)
+    }
+    getResult(){
+        console.log(`English = ${this.english} Math = ${this.math}`)
+        let total = this.english + this.math
+        let percentage = (total/100) * 100
+        console.log(`percentage = ${percentage}`)
+    }
+}
+
+let S1 = new Students("ashirwad",5510,"ai",25,15)
+let S2 = new Students("Dhol",420,"accounting")
+S1.getGRID()
+S1.getResult()
